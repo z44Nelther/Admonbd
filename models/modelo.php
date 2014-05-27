bb9b5be8b64936bd44ee9d035f97126232ec396b
z44Nelther <z44.nelther@gmail.com>
2014-05-26 20:52:40 -0500

    ejecutando consulta

--- models/modelo.php
+++ models/modelo.php
@@ -7,7 +7,7 @@ function Modelo() {
 	
 public function consulta_datos() {
 	echo "Consultando";
-	echo "".$c;
+	
 	$stid = oci_parse($this->c, 'SELECT * FROM' . $this->nombre_tabla);
 	echo "Consultando-Select";
 	$rs=oci_execute($stid);
