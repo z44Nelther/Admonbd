c1f2c7b4da7abbc81b434f2b58e563f45ee66ef9
Nelther <z44.nelther@gmail.com>
2014-05-27 03:40:51 -0500

    adodb

--- models/modelo.php
+++ models/modelo.php
@@ -4,9 +4,12 @@ class Modelo extends Conexion {
 function Modelo() {
         parent::Conexion();
     }
+    
 	public function consulta_datos() {
         $rs = $this->db->Execute('SELECT * from ' . $this->nombre_tabla);
-        $this->get_error($rs, 'Error en consulta datos');
+        //$this->get_error($rs, 'Error en consulta datos');
         return $rs;
     }
+}
+    
 ?>
