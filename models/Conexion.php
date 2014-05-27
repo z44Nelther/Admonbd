136b13bcb3e68d512edfc13934c7c121f953331a
z44Nelther <z44.nelther@gmail.com>
2014-05-26 20:16:21 -0500

    ejecutando consulta

--- models/Conexion.php
+++ models/Conexion.php
@@ -1,8 +1,10 @@
 <?php
-class Conexion {
-	error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL|E_STRICT);
 	ini_set('display_errors', 'On'); 
 	oci_internal_debug(1);
+	
+class Conexion {
+	
 	
 		function Conexion(){
 		$c = oci_pconnect("prac51","prac51","127.0.0.1/admonbd2");
