58d6bc61b62edd82d5ce1c379f401aa2c6e10b3a
Nelther <z44.nelther@gmail.com>
2014-05-27 05:12:18 -0500

    adodb

--- models/Conexion.php
+++ models/Conexion.php
@@ -1,6 +1,6 @@
 <?php
-    error_reporting(E_ALL|E_STRICT);
-    ini_set('display_errors', 'On');
+    //error_reporting(E_ALL|E_STRICT);
+   // ini_set('display_errors', 'On');
     //oci_internal_debug(1);
    
 class Conexion {
