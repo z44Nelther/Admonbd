a75b535dfef819471a044d98b6e85484cdfc96b8
Nelther <z44.nelther@gmail.com>
2014-05-27 04:33:28 -0500

    adodb

--- models/modelo.php
+++ models/modelo.php
@@ -6,7 +6,7 @@ function Modelo() {
     }
     
 	public function consulta_datos() {
-        $rs = $this->db->Execute('SELECT * from ' . $this->nombre_tabla);
+        $rs = $this->db->Execute('SELECT * from ' . $this->nombre_tabla .';');
         //$this->get_error($rs, 'Error en consulta datos');
         return $rs;
     }
