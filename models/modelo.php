5b4b29ab1cd3742362a01bd73525a6818b0a4c37
z44Nelther <z44.nelther@gmail.com>
2014-05-26 21:30:30 -0500

    ejecutando consulta

--- models/modelo.php
+++ models/modelo.php
@@ -1,14 +1,21 @@
 <?php
-class Modelo extends Conexion {
+class Modelo  {
+
+	public $c;
 
 function Modelo() {
-        parent::Conexion();
+        $c=oci_pconnect("prac51","prac51","127.0.0.1/admonbd2");
+        if (!$c) {
+			echo "error con la conexion";	   
+		}
+
+
     }
 	
 public function consulta_datos() {
 	echo "Consultando";
 	
-	$stid = oci_parse($this->get_c(), 'SELECT * FROM' . $this->nombre_tabla);
+	$stid = oci_parse($c, 'SELECT * FROM' . $this->nombre_tabla);
 	echo "Consultando-Select";
 	$rs=oci_execute($stid);
 	return $rs;
