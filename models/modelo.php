ca0fa59477bd4d71abdb416155ea95ea70d5eede
z44Nelther <z44.nelther@gmail.com>
2014-05-26 21:48:08 -0500

    ejecutando consulta

--- models/modelo.php
+++ models/modelo.php
@@ -3,10 +3,11 @@ error_reporting(E_ALL|E_STRICT);
 	ini_set('display_errors', 'On'); 
 	oci_internal_debug(1);
 
+ $c=oci_pconnect("prac51","prac51","127.0.0.1/admonbd2");
 class Modelo  {
 
-	public $c=oci_pconnect("prac51","prac51","127.0.0.1/admonbd2");
-
+	
+	
 function Modelo() {
         
         if (!$c) {
