fa5778c2154c16c54693f03ca6eddaa4d4b08f63
z44Nelther <z44.nelther@gmail.com>
2014-05-26 20:47:59 -0500

    ejecutando consulta

--- models/Conexion.php
+++ models/Conexion.php
@@ -8,6 +8,7 @@ class Conexion {
 	
 		function Conexion(){
 		$c = oci_pconnect("prac51","prac51","127.0.0.1/admonbd2");
+		echo "".$c; 
 				  //$c = ocilogon("prac51","prac51","admonbd2");
 		if (!$c) {
 			echo "error con la conexion";	   
