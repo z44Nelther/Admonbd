051651a99ac84673ef8a972d189c07e887d255dc
Nelther <z44.nelther@gmail.com>
2014-05-27 03:32:46 -0500

    adodb

--- models/Conexion.php
+++ models/Conexion.php
@@ -1,4 +1,7 @@
 <?php
+    error_reporting(E_ALL|E_STRICT);
+    init_set('display_errors', 'On');
+    
     class Conexion {
         function Conexion(){
             $this->db=ADONewConnection('oci8');
