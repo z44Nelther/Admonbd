a243c41c99c60ce864090a4c9b302d677757ff6f
z44nelther <z44.nelther@gmail.com>
2014-06-01 15:57:39 -0500

    join team

--- views/site/equipos.php
+++ views/site/equipos.php
@@ -32,7 +32,7 @@
              <tr> <form  method="POST">
                     <td>
 					    <div class="form-group">
-					    <input class="" name="username" type="text" value="<?php echo $row['NOM_EQUIPO']; ?>" readonly>
+					    <input class="form-control" style="background-color:transparent; " name="username" type="text" value="<?php echo $row['NOM_EQUIPO']; ?>" readonly>
 						</div>
                      </td>   
                   </form>
