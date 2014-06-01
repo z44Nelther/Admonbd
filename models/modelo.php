9eb8ed5392ff288259ec11e18c421d6a072ac742
z44nelther <z44.nelther@gmail.com>
2014-06-01 18:10:51 -0500

    join team

--- models/modelo.php
+++ models/modelo.php
@@ -25,10 +25,10 @@ function Modelo() {
     
         public function Unir_equipo($id_user,$id_eq){
             $stmt = $this->db->PrepareSP("BEGIN adodb.UnirseEq(:a1, :a2, :a3); END;");
-            $db->InParameter($stmt,$id_user,'a1');
-            $db->InParameter($stmt,$id_eq,'a2');
-            $db->OutParameter($stmt,$output,'a3');
-            $ok = $db->Execute($stmt);
+            $this->db->InParameter($stmt,$id_user,'a1');
+            $this->db->InParameter($stmt,$id_eq,'a2');
+            $this->db->OutParameter($stmt,$output,'a3');
+            $ok = $this->db->Execute($stmt);
            if($ok){
                echo "CORRECTO";
            }else{
