fecab1c67fa5e66ba8d430032f121668a1b35cf3
z44nelther <z44.nelther@gmail.com>
2014-06-01 18:22:54 -0500

    join team

--- models/modelo.php
+++ models/modelo.php
@@ -24,7 +24,7 @@ function Modelo() {
         }
     
         public function Unir_equipo($id_user,$id_eq){
-            $stmt = $this->db->PrepareSP("BEGIN adodb.UnirseEq(:a1, :a2, :a3); END;");
+            $stmt = $this->db->PrepareSP("BEGIN UnirseEq(:a1, :a2, :a3); END;");
             $this->db->InParameter($stmt,$id_user,'a1');
             $this->db->InParameter($stmt,$id_eq,'a2');
             $this->db->OutParameter($stmt,$output,'a3');
