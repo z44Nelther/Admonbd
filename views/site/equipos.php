7b4a62fc665ef87f45c897183183ae3bead2c007
z44nelther <z44.nelther@gmail.com>
2014-06-01 20:29:27 -0500

    join team

--- views/site/equipos.php
+++ views/site/equipos.php
@@ -25,7 +25,7 @@ if (isset($_POST["nom_eq"])) {
 	<div class="col-md-12 text-center" id="maindiv">
 		<div class="col-md-8">
 		<div class="">
-			<h1 class="text-left proxE">Lista de equipos</h1>
+			<h1 class="text-left proxE">Lista de equipos</h1><a class="btn btn-default" href="#" role="button">Link</a>
 		</div>
 		<table class="table table-striped">
 			<thead class="theader" >
