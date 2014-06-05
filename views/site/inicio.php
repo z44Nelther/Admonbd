66a193509b0cafacdca78bf84951a77b2556749e
z44Nelther <z44.nelther@gmail.com>
2014-06-04 23:29:05 -0500

    login

--- views/site/inicio.php
+++ views/site/inicio.php
@@ -1,4 +1,13 @@
 <?php session_start();
+   include ('../../libs/adodb5/adodb-pager.inc.php');
+      include ('../../libs/adodb5/adodb.inc.php');
+      include ('../../models/Conexion.php');
+      include ('../../models/modelo.php');
+      include ('../../models/Usuario.php');
+      include ('../../controllers/siteController/LoginController.php');
+      include ('../../libs/er.php');
+     
+
   
   include ('../layouts/header.php');
   
