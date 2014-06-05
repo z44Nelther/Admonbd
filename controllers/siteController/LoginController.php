7ff40bc30e95e9af336da2638af05b65b2701a9f
z44Nelther <z44.nelther@gmail.com>
2014-06-04 20:09:27 -0500

    login

--- controllers/siteController/LoginController.php
+++ controllers/siteController/LoginController.php
@@ -6,11 +6,12 @@
 			 parent::Usuario();
 		}
 		public function validaUsuario($datos){
-			$rs = $this->consulta_sql(" select * from usuario where email = '".$datos['nom_usuario']."'  ");
+			$rs = $this->consulta_sql(" select * from usuario where nom_usuario = '".$datos['nom_usuario']."'  ");
         	$rows = $rs->GetArray();
         	if(count($rows) > 0){
         		if ($rows['0']['password']== $datos['password']) {
-        			$this->iniciarSesion($rows['0']['nom_usuario'],$rows['0']['nom_usuario']);
+        		
+        			$this->iniciarSesion($rows['0']['nom_usuario'],$rows['0']['nom_usuario']);echo "INICIANDO :D";
         		}else{
 		     		$this->muestra_errores = true;
 		     		$this->errores[] = 'Password incorrecto';
